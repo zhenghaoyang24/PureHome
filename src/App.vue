0495a41a380be0c4945c8190f388b36523bf9144
zhenghaoyang24 <zhenghaoyang24@foxmail.com>
2025-09-02 11:39:53 +0800

    0.0.0

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import SearchInput from './components/SearchInput.vue'
+</script>
+
+<template>
+  <div class="search-container"></div>
+  <div class="tools-container"></div>
+</template>
+
+<style scoped>
+.search-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background-image: url('https://api.xsot.cn/bing?jump=true');
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.tools-container {
+  position: relative;
+  width: 100%;
+  height: 100vh;
+  background-color: rgba(243, 0, 0, 0.5);
+}
+</style>
